0f2ebea40605179e67c53fc6dffd9d27781a3694
somarrod <107022959+somarrod@users.noreply.github.com>
2026-05-13 19:23:03 +0200

    resolt error de compilació

--- Serveis/Interfaces/ICategoriaService.cs
+++ Serveis/Interfaces/ICategoriaService.cs
@@ -7,14 +7,14 @@ namespace KarmaWebAPI.Serveis.Interfaces
 {
     public interface ICategoriaService
     {
-        Task<Categoria?> InstanciaAsync(long idCategoria, ClaimsPrincipal user);
-        Task<List<Categoria>> LlistaAsync(ClaimsPrincipal user);
+        Task<CategoriaDisplaySet?> InstanciaAsync(long idCategoria, ClaimsPrincipal user);
+        Task<List<CategoriaDisplaySet>> LlistaAsync(ClaimsPrincipal user);
 
         Task<Categoria> CrearAsync(CategoriaCrearDTO dto);
         Task<Categoria> EditarAsync(CategoriaEditarDTO dto);
 
-        Task<CategoriaDisplaySet> ActivarAsync(long idCategoria);
-        Task<CategoriaDisplaySet> DesactivarAsync(long idCategoria);
+        Task<Categoria> ActivarAsync(long idCategoria);
+        Task<Categoria> DesactivarAsync(long idCategoria);
 
         Task<bool> EliminarAsync(long idCategoria);
     }
